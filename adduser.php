8bb0c54d01ac7245991079ac5a7c37f9eb524f3c
Gurwinder Singh <Gurwinder Singh>
2017-04-25 21:23:10 +0530

    deleteing clear

--- adduser.php
+++ adduser.php
@@ -4,22 +4,12 @@
 	<input type="submit" name="submit" value="Sign Up" />
 </form>
 <?php
-function clear($message)
-{
-	if(!get_magic_quotes_gpc())
-	{
-		$message = addslashes($message);
-	}
-	$message = strip_tags($message);
-	$message = htmlentities($message);
-	return trim($message);
-}
 if ($_POST['submit'])
 {
 	mysql_connect('localhost','root','root');
 	mysql_select_db('sampledb');
-	$name = clear($_POST['name']); 
-	$password = clear($_POST['password']);
+	$name = $_POST['name']; 
+	$password = $_POST['password'];
 	$sql = mysql_query("SELECT * FROM user WHERE name = '$name'");
 	if (!mysql_fetch_array($sql))
 	{
