4b8ca3fc33047e1ef6531f8d6a6ef327b044fa7b
Gurwinder Singh <Gurwinder Singh>
2017-04-25 19:17:41 +0530

    Ajax

--- test.php
+++ test.php
@@ -1,5 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
 <?php
-echo "<h2>Test in H2 Tag</h2>";
-echo "Hello world! break tag<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+$q = intval($_GET['q']);
+
+$con = mysqli_connect('localhost','root','root','sampledb');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"ajax_demo");
+$sql="SELECT * FROM test WHERE id = '".$q."'";
+$result = mysqli_query($con,$sql);
+
+echo "<table>
+<tr>
+<th>Id</th>
+<th>Name</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
 ?>
+</body>
+</html>
